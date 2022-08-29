1c690607b733d04bcba11a0cad194c4235ae6ee2
DylanKoehler <dylankoehler3212@gmail.com>
2022-08-29 12:14:35 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Dylan Koehler on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
