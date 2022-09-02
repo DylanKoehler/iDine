69d1c33e498d6033f716a0e7c875d95bb587921d
DylanKoehler <dylankoehler3212@gmail.com>
2022-09-02 09:33:20 -0500

    Completed Step 9.

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,34 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Dylan Koehler on 9/2/22.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = "Cash"
+    
+    let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView().environmentObject(Order())
+    }
+}
